9de01598ee73c4020b6e02143429ab8fa42e21f3
sanri95 <santirio95@gmail.com>
2024-03-10 19:50:53 -0500

    se cre el Footer

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export const Footer = () => {
+  return (
+    <>
+    <div id="pagina">
+  <div id="contenido"></div>
+  <div id="pie">2024 SAYE</div>
+  <div id="pie">SAYE &copy;</div>
+</div>
+</>
+  )
+};
